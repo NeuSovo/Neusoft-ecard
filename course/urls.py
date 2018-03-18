49b9ab559ab31a1b8e5908dd2331e83ba9f5b2f4
zzde <zhangxh1997@gmail.com>
2018-03-18 21:04:49 +0800

    clean dev

--- course/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-urlpatterns = [
-    url(r'^empty$',views.get_empty_class),
-    url(r'^rubc$',views.rub_class),
-    url(r'^rubt$',views.rub_teachter),
-    ]
